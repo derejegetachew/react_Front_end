f2ef213c679b0af7006b1fea92c6edde84944480
Dereje Getachew <101693929+derejegetachew@users.noreply.github.com>
2024-11-21 13:23:41 +0300

    add jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Functional_componet from "./Componets/Functional_componet";
+import Hello from "./Componets/Class_componet";
+import Wellcome_jsx from "./Componets/Wellcome_jsx";
 function App() {
   return (
     <div className="App">
       <Functional_componet />
+      <Hello />
+      <Wellcome_jsx />
     </div>
   );
 }
---
+++ src/Componets/Wellcome_jsx.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Wellcome_jsx = () => {
+  //   return (
+  //     <div>
+  //       <h2> well come to jsx</h2>
+  //     </div>
+  //   );
+  // it  not use  jsx
+  return React.createElement(
+    "div",
+    { id: "Wellcom", className: "weell" },
+    React.createElement("h1", { id: "h1" }, "well come to you")
+  );
+};
+
+export default Wellcome_jsx;
